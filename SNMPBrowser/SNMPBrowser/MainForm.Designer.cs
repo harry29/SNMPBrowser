d31c5211e53aa4ff20388dde662c1e7804874591
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2016-12-06 13:31:56 +0100

    Implemented application view.

---
+++ SNMPBrowser/SNMPBrowser/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SNMPBrowser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.operationComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.goButton = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.removeTabButton = new System.Windows.Forms.Button();
+            this.oidTextBox = new System.Windows.Forms.TextBox();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(12, 41);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(202, 388);
+            this.treeView.TabIndex = 0;
+            // 
+            // operationComboBox
+            // 
+            this.operationComboBox.FormattingEnabled = true;
+            this.operationComboBox.Items.AddRange(new object[] {
+            "GetRequest",
+            "GetNextRequest",
+            "Observe"});
+            this.operationComboBox.Location = new System.Drawing.Point(297, 14);
+            this.operationComboBox.Name = "operationComboBox";
+            this.operationComboBox.Size = new System.Drawing.Size(140, 21);
+            this.operationComboBox.TabIndex = 2;
+            this.operationComboBox.Text = "GetRequest";
+            this.operationComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Operation:";
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(443, 12);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 4;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Location = new System.Drawing.Point(220, 41);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(392, 388);
+            this.tabControl.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(384, 362);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(384, 362);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // removeTabButton
+            // 
+            this.removeTabButton.Location = new System.Drawing.Point(524, 12);
+            this.removeTabButton.Name = "removeTabButton";
+            this.removeTabButton.Size = new System.Drawing.Size(84, 23);
+            this.removeTabButton.TabIndex = 5;
+            this.removeTabButton.Text = "Remove Tab";
+            this.removeTabButton.UseVisualStyleBackColor = true;
+            this.removeTabButton.Click += new System.EventHandler(this.removeTabButton_Click);
+            // 
+            // oidTextBox
+            // 
+            this.oidTextBox.Enabled = false;
+            this.oidTextBox.Location = new System.Drawing.Point(12, 14);
+            this.oidTextBox.Name = "oidTextBox";
+            this.oidTextBox.Size = new System.Drawing.Size(202, 20);
+            this.oidTextBox.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.oidTextBox);
+            this.Controls.Add(this.removeTabButton);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.operationComboBox);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.treeView);
+            this.Name = "MainForm";
+            this.Text = "SNMP Browser";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ComboBox operationComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button removeTabButton;
+        private System.Windows.Forms.TextBox oidTextBox;
+    }
+}
+
---
+++ SNMPBrowser/SNMPBrowser/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SNMPBrowser
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void goButton_Click(object sender, EventArgs e) {
+
+        }
+
+        private void removeTabButton_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
