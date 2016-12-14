2da13df48de17a82b6c47d7fa97c5502d563ecc6
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2016-12-14 11:35:28 +0100

    Changed view. User can now clear tables instead of removing them.

--- SNMPBrowser/SNMPBrowser/MainForm.cs
+++ SNMPBrowser/SNMPBrowser/MainForm.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 using SnmpSharpNet;
 using SNMPBrowser.Properties;
 
 namespace SNMPBrowser {
     public partial class MainForm : Form {
-        private DataGridView _dataGridView;
-        private DataGridView _trapGridView;
-
-        readonly TabPage _trapTabPage = new TabPage("Trap Receiver");
-
         private SnmpClient _snmpClient;
         private string _selectedOperation;
 
@@ -23,46 +17,14 @@ namespace SNMPBrowser {
 
         public MainForm() {
             InitializeComponent();
-            CreateDataGrid();
-            CreateTrapGridView();
-            CreateSnmpClient();
-        }
-
-        private void CreateDataGrid() {
-            _dataGridView = new DataGridView();
-            _dataGridView.Columns.Add("oidColumn", "OID");
-            _dataGridView.Columns.Add("valueColumn", "Value");
-            _dataGridView.Columns.Add("typeColumn", "Type");
-            _dataGridView.Size = tabControl.SelectedTab.Size;
-            tableTabPage.Controls.Add(_dataGridView);
-            _dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
-        }
-
-        private void CreateTrapGridView() {
-            _trapGridView = new DataGridView();
-            _trapGridView.Columns.Add("versionColumn", "SNMP Version");
-            _trapGridView.Columns.Add("oidColumn", "OID");
-            _trapGridView.Columns.Add("valueColumn", "Value");
-            _trapGridView.Columns.Add("typeColumn", "Type");
-            _trapGridView.Columns.Add("timeColumn", "Time");
-            _trapGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
-
-        }
-
-        private void CreateSnmpClient() {
-            _snmpClient = new SnmpClient();
-            _snmpClient.OnTrapRecieved += (sender, snmpPacket) => BeginInvoke(new Action(() => ReceiveTrap(sender, snmpPacket)));
-        }
-
-        private void ReceiveTrap(object sender, SnmpPacket snmpPacket) {
-            foreach (var vb in snmpPacket.Pdu.VbList) {
-                _trapGridView.Rows.Add(snmpPacket.Version, vb.Oid.ToString(), vb.Value.ToString(), SnmpConstants.GetTypeName(vb.Value.Type),
-                    DateTime.Now.ToString("G"));
-            }
-        }
-
-        private void MainForm_Load(object sender, EventArgs e) {
+            InitializeSnmpClient();
             operationComboBox_Initialize();
+        }
+
+        private void InitializeSnmpClient() {
+            _snmpClient = new SnmpClient();
+            _snmpClient.OnTrapRecieved +=
+                (sender, snmpPacket) => BeginInvoke(new Action(() => ReceiveTrap(sender, snmpPacket)));
         }
 
         private void operationComboBox_Initialize() {
@@ -71,15 +33,23 @@ namespace SNMPBrowser {
             operationComboBox.Items.Add(GetTable);
             operationComboBox.Items.Add(Listen);
             operationComboBox.Items.Add(Monitor);
-            
+
             operationComboBox.SelectedItem = GetRequest;
+        }
+
+        private void ReceiveTrap(object sender, SnmpPacket snmpPacket) {
+            foreach (var vb in snmpPacket.Pdu.VbList) {
+                trapListenerDataGridView.Rows.Add(vb.Oid.ToString(), vb.Value.ToString(),
+                    SnmpConstants.GetTypeName(vb.Value.Type),
+                    DateTime.Now.ToString("G"), snmpPacket.Version);
+            }
         }
 
         private void comboBox_SelectedIndexChanged(object sender, EventArgs e) {
             _selectedOperation = operationComboBox.SelectedItem.ToString();
         }
 
-        private void ShowResult(Dictionary<Oid, AsnType> result) {
+        private void ShowResult(Dictionary<Oid, AsnType> result, DataGridView target) {
             if (result == null) {
                 ErrorMessageBox.Show("Request failed.");
             }
@@ -89,8 +59,8 @@ namespace SNMPBrowser {
                         ErrorMessageBox.Show("Request failed.");
                     }
                     else {
-                        _dataGridView.Rows.Add(entry.Key.ToString(), entry.Value.ToString(),
-                            SnmpConstants.GetTypeName(entry.Value.Type));
+                         target.Rows.Add(entry.Key.ToString(), entry.Value.ToString(),
+                                         SnmpConstants.GetTypeName(entry.Value.Type), DateTime.Now.ToLongTimeString());
                     }
                 }
             }
@@ -99,26 +69,19 @@ namespace SNMPBrowser {
         private void goButton_Click(object sender, EventArgs e) {
             switch (_selectedOperation) {
                 case GetRequest:
-                    ShowResult(_snmpClient.GetRequest(oidTextBox.Text));
+                    ShowResult(_snmpClient.GetRequest(oidTextBox.Text) , requestTableDataGridView);
                     break;
                 case GetNextRequest:
-                    ShowResult(_snmpClient.GetNextRequest(oidTextBox.Text));
+                    ShowResult(_snmpClient.GetNextRequest(oidTextBox.Text), requestTableDataGridView);
                     break;
                 case GetTable:
-                    ShowResult(_snmpClient.GetTable(oidTextBox.Text));
+                    ShowResult(_snmpClient.GetTable(oidTextBox.Text), requestTableDataGridView);
                     break;
                 case Monitor:
                     MonitorObject(oidTextBox.Text);
                     break;
                 case Listen:
                     _snmpClient.Listen();
-
-                    tabControl.TabPages.Add(_trapTabPage);
-                    _trapTabPage.Controls.Add(_trapGridView);
-                    _trapGridView.Size = _trapTabPage.Size;
-                    _trapTabPage.Controls.Add(_trapGridView);
-
-                    tabControl.SelectedTab = _trapTabPage;
                     break;
             }
         }
@@ -127,23 +90,17 @@ namespace SNMPBrowser {
             var timer = new Timer {Interval = Settings.Default.MonitorInterval};
             timer.Tick += (sender, eventArgs) => OnTick(sender, eventArgs, oid);
             timer.Start();
-            //TODO List of running tasks so user could stop them. Maybe binding TabPanel with actions?
         }
 
         private void OnTick(object sender, EventArgs e, string oid) {
-            ShowResult(_snmpClient.GetRequest(oid));
+            ShowResult(_snmpClient.GetRequest(oid), monitorDataGridView);
         }
 
         private void removeButton_Click(object sender, EventArgs e) {
             var selectedTab = tabControl.SelectedTab;
 
-            if (selectedTab.Equals(tableTabPage)) {
-                selectedTab.Controls.Remove(_dataGridView);
-                CreateDataGrid();
-            }
-            else {
-                tabControl.TabPages.Remove(selectedTab);
-            }
+            var selectedTabDataGridView = selectedTab.Controls[0] as DataGridView;
+            selectedTabDataGridView?.Rows.Clear();
         }
     }
 }
