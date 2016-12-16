397cdd46e1c27c822b8c5cd41d0ddc6fb583df2f
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2016-12-16 22:55:56 +0100

    Extended Oid with FindRoot method.

---
+++ SNMPBrowser/SNMPBrowser/OidExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnmpSharpNet;
+
+namespace SNMPBrowser {
+    public static class OidExtension {
+        public static Oid FindRoot(this Oid thisOid, Oid otherOid) {
+            var thisOidString = thisOid.ToString();
+            var otherOidString = otherOid.ToString();
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < thisOidString.Length; i++) {
+                if (thisOidString[i].Equals(otherOidString[i])) {
+                    sb.Append(thisOidString[i]);
+                }
+                else break;
+            }
+
+            var rootOidString = sb.ToString().TrimEnd('.');
+            return new Oid(rootOidString);
+        }
+    }
+}
--- SNMPBrowser/SNMPBrowserTest/SNMPClientTest.cs
+++ SNMPBrowser/SNMPBrowserTest/SNMPClientTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnmpSharpNet;
 using SNMPBrowser;
 
 namespace SNMPBrowserTest {
@@ -14,13 +16,23 @@ namespace SNMPBrowserTest {
         [TestMethod]
         public void GetTableTest() {
             SnmpClient snmpClient = new SnmpClient();
-            var table = snmpClient.GetTable(".1.3.6.1.2.1.6.13");
+            ShowGetTableResult(snmpClient.GetTable(".1.3.6.1.2.1.6.13"));
         }
 
         [TestMethod]
         public void ListenTest() {
             SnmpClient snmpClient = new SnmpClient();
             snmpClient.Listen();
+        }
+
+        private void ShowGetTableResult(Dictionary<Oid, AsnType> result) {
+            List<KeyValuePair<Oid, AsnType>> resultList = new List<KeyValuePair<Oid, AsnType>>(result);
+            for (int i = 0; i < resultList.Count - 1; i++) {
+                var prev = resultList[i];
+                var next = resultList[i + 1];
+
+                Console.WriteLine(prev.Key.FindRoot(next.Key));
+            }
         }
     }
 }
